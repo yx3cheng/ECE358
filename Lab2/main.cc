0285c7e6265723e73f463bbb77b429339a4c9086
Jack Yang <jy7.yang@gmail.com>
2015-07-11 20:08:58 -0400

    fix rand, seg fault

--- Lab2/main.cc
+++ Lab2/main.cc
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <cmath>
-#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
 #include <vector>
 #include <queue>
-
 using namespace std;
+
 #define IDLE 0
 #define SENSING 1
 #define TRANSMIT 2
@@ -13,172 +13,175 @@ using namespace std;
 #define BUSY 5
 #define COLLISION 6
 
-#define ONE_PERSISTENT 1
-//#define NON_PERSISTENT 1
-//#define PRB_PERSISTENT 1
-
 struct Packet {
-    int m_generated_time;
-    int m_i;
+  int m_generated_time;
+  int m_i;
 
-
-    Packet(int a) : m_generated_time(a) {}
+  Packet(int generatedTime) : m_generated_time(generatedTime), m_i(0) {}
 };
 
 struct Node {
-    int m_id;
-    int m_state;
-    int m_nextState;
-    int m_time;
-    int m_genTime;
-    int m_transTime;
+  int m_id;
+  int m_state;
+  int m_nextState;
+  int m_time;
+  long long m_genTime;
+  int m_transTime;
 
-    queue<Packet*> m_packet_queue;
+  queue<Packet*> m_packet_queue;
 };
-
 
 int busy_counter = 0;
 int successfulPackets = 0;
 int T = 10;
 int ticksPerSecond = 1000000;
+static std::vector<Node*> nodes;
 
 struct Generator {
-    double lambda;
+  double lambda;
 
-    Generator() {}
-    Generator(double lambda): lambda(lambda) {}
-    int generateExpRandomNum() {
-        return (int)(((-1 / lambda) * log(1 - ((double)rand() / RAND_MAX))) * ticksPerSecond);
+  Generator() {
+    srand(time(NULL));
+  }
+  double generateExpRandomNum() {
+    double U = ((double) (rand() % 1000) / 1000);
+    return ((-1 / lambda) * log(1 - U));
+  }
+} generator;
+
+int medium_sense(struct Node* a_node) {
+  int busy_counter = 0;
+
+  int transmitting_id;
+  for (int i = 0; i < nodes.size(); i++) {
+    if (nodes[i]->m_state == TRANSMIT) {
+      if (a_node->m_id != nodes[i]->m_id)
+        transmitting_id = i;
+      busy_counter++;
     }
-};
+  }
+  // Calculate wire distance between transmitting station and current station.
+  int distance = abs(transmitting_id - a_node->m_id) * 10;
 
-std::vector<Node*> nodes;
+  if (busy_counter == 0) {
+    return IDLE;
+  } else if (busy_counter == 1) {
+    return BUSY;
+  } else {
+    return COLLISION;
+  }
+}
 
-Generator generator;
+void tick(struct Node* a_node) {
+  a_node->m_time++;
 
-int medium_Sense (struct Node* a_node) {
-    int busy_counter = 0;
+  switch (a_node->m_state) {
+    case IDLE:
+      if (!a_node->m_packet_queue.empty()) {
+        a_node->m_nextState = SENSING;
+      }
+      break;
 
-    int transmitting_id;
-    for (int i = 0; i < nodes.size(); i++) {
-        if (nodes[i]->m_state == TRANSMIT) {
-            transmitting_id = i;
+    case SENSING:
+#ifdef ONE_PERSISTENT
+      if (a_node->m_time == 96 * ticksPerSecond) {
+        a_node->m_time = 0;
+        if (medium_sense(a_node) != IDLE) {
+          a_node->m_state = TRANSMIT;
+          a_node->m_transTime = 0; // ???
         }
-    }
-    if (busy_counter == 0) {
-        return IDLE;
-    } else if (busy_counter == 1) {
-        return BUSY;
-    } else {
-        return COLLISION;
-    }
-}
-
-
-void updateSimulation (struct Node* a_node) {
-    a_node->m_time++;
-
-    switch (a_node->m_state) {
-        case IDLE:
-            if (!a_node->m_packet_queue.empty()) {
-                a_node->m_nextState = SENSING;
-            }
-            break;
-        case SENSING:
-        #ifdef ONE_PERSISTENT
-            if (a_node->m_time == 96 * ticksPerSecond) {
-                a_node->m_time = 0;
-                if (medium_Sense(a_node) != IDLE) {
-                	a_node->m_state = TRANSMIT;
-                	a_node->m_transTime =
-                }
-            }
-        #elif NON_PERSISTENT
-            if (a_node->m_time == 96 * ticksPerSecond) {
-                a_node->m_time = 0;
-                if (medium_Sense(a_node) != IDLE) {
-                	a_node->m_state = TRANSMIT;
-                } else {
-                	// wait randomly and sense again.
-                }
-            }
-        #elif PRB_PERSISTENT
-
-        #endif
-            break;
-        case TRANSMIT:
-            // TODO calculate transmission time
-            if (medium_Sense() == COLLISION) {
-                a_node->m_state = JAMMING;
-                a_node->m_time = 0;
-            } else {
-                successfulPackets++;
-                a_node->m_state = IDLE;
-            }
-            break;
-        case JAMMING:
-            if (a_node->m_time == 48 * ticksPerSecond) {
-                a_node->m_state = BACKOFF;
-                a_node->m_time = 0;
-            }
-            break;
-        case BACKOFF:
-            if (a_node->m_time == (rand() % pow(2,i)) * 512 * ticksPerSecond) {
-                a_node->m_time = 0;
-                a_node->m_state = SENSING;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-void Start_simulation (int a_totalticks,int a_N,int a_A,int a_W,int a_L) {
-    for (int current_tick = 0; current_tick <= a_totalticks; current_tick++) {
-        for (int n = 0; n <= a_N; n++) { // loop for number of nodes
-
-            if (nodes[n]->m_genTime == current_tick) { // generate new packet
-                nodes[n]->m_packet_queue.push(new Packet(current_tick));
-                nodes[n]->m_genTime = current_tick + generator.generateExpRandomNum();
-            }
-
-            updateSimulation(nodes[n]);
+      }
+#elif NON_PERSISTENT
+      if (a_node->m_time == 96 * ticksPerSecond) {
+        a_node->m_time = 0;
+        if (medium_sense(a_node) != IDLE) {
+          a_node->m_state = TRANSMIT;
+        } else {
+          // wait randomly and sense again.
         }
-    }
+      }
+#elif PRB_PERSISTENT
+
+#endif
+      break;
+
+    case TRANSMIT:
+    // TODO calculate transmission time
+      if (medium_sense(a_node) == COLLISION) {
+        a_node->m_state = JAMMING;
+        a_node->m_time = 0;
+      } else {
+        successfulPackets++;
+        a_node->m_state = IDLE;
+      }
+      break;
+
+    case JAMMING:
+      if (a_node->m_time == 48 * ticksPerSecond) {
+        a_node->m_state = BACKOFF;
+        a_node->m_time = 0;
+      }
+      break;
+
+    case BACKOFF:
+      if (a_node->m_time
+            == rand() % (int) pow(2, a_node->m_packet_queue.front()->m_i) * 512 * ticksPerSecond) {
+        a_node->m_time = 0;
+        a_node->m_state = SENSING;
+      }
+      break;
+
+    default:
+      break;
+  }
 }
 
-void Compute_performances () {
-    double throughput = successfulPackets / T;
+void start_simulation (int a_totalticks,int a_N,int a_A,int a_W,int a_L) {
+  for (int current_tick = 0; current_tick <= a_totalticks; current_tick++) {
+    for (int n = 0; n < a_N; n++) { // loop for number of nodes
+
+      if (nodes[n]->m_genTime == current_tick) { // generate new packet
+        nodes[n]->m_packet_queue.push(new Packet(current_tick));
+        nodes[n]->m_genTime = current_tick + generator.generateExpRandomNum() * ticksPerSecond;
+      }
+
+      tick(nodes[n]);
+    }
+  }
+}
+
+void compute_performances () {
+  // double throughput = successfulPackets / T;
 }
 
 int main(int argc, char** argv) {
-    int N, A, W, L, P = 0;
-    if (argc != 5 && argc != 6) {
-        cout << "Usage: N A W L [P]\n";
-        return 0;
-    }
-    N = atoi(argv[1]);
-    A = atoi(argv[2]);
-    W = atoi(argv[3]);
-    L = atoi(argv[4]);
-    if(argc == 6)
-        P = atoi(argv[5]);
+  int N, A, W, L, P = 0;
+  if (argc != 5 && argc != 6) {
+    cout << "Usage: N A W L [P]\n";
+    return 0;
+  }
+  N = atoi(argv[1]); // Number of nodes
+  A = atoi(argv[2]); // Arrival rate
+  W = atoi(argv[3]); // LAN throughput in bits per second
+  L = atoi(argv[4]); // Packet length in bits
+  if (argc == 6)
+    P = atoi(argv[5]); // persistence parameter
 
-    // set generator lamda
-    generator.lambda = A;
+  // set generator lamda
+  generator.lambda = A;
 
-    // nodes
-    for (int i = 0; i < N; i++) {
-        Node* node = new Node[sizeof(Node)];
-        node->m_id = i;
-        node->m_state = IDLE;
-        node->m_genTime = generator.generateExpRandomNum();
-        nodes.push_back(node);
-    }
+  // nodes
+  for (int i = 0; i < N; i++) {
+    Node* node = new Node();
+    node->m_id = i;
+    node->m_state = IDLE;
+    node->m_genTime = generator.generateExpRandomNum() * ticksPerSecond;
+    nodes.push_back(node);
+  }
 
-    Start_simulation (T*ticksPerSecond,N,A,W,L);
-    Compute_performances();
+  start_simulation(T * ticksPerSecond, N, A, W, L);
+  // compute_performances();
 
-    for (int i = 0; i < N; i++)
-        delete nodes[i];
+  for (int i = 0; i < N; i++)
+    delete nodes[i];
 }
